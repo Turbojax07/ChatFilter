18f78636ba217a84abccb9e6f05420680b3a36c7
turbojax07 <turbojax07@gmail.com>
2025-10-24 07:19:22 -0400

    Adding implementations of all the commands

--- src/main/java/org/turbojax/ChatFilterCommand.java
+++ src/main/java/org/turbojax/ChatFilterCommand.java
@@ -1,9 +1,8 @@
 package org.turbojax;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import java.util.Map;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -23,9 +22,9 @@ public class ChatFilterCommand implements TabExecutor {
         if (args.length == 0) {
             // Checking permissions
             if (!sender.hasPermission("chatfilter.command.help")) {
-            Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
+                Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
                 return true;
-        }
+            }
 
             Message.send(sender, Message.COMMAND_HELP_MESSAGE, placeholders);
             return true;
@@ -64,32 +63,73 @@ public class ChatFilterCommand implements TabExecutor {
                 MainConfig.load();
                 Message.load();
 
-                // Redownloading the wordlist if configs allow
-                if (MainConfig.useRemoteWordlist()) {
-                    WordlistManager.redownload();
-                }
+                // Reloading wordlist
+                WordlistManager.reload();
+                Message.send(sender, Message.COMMAND_RELOAD_SUCCESS, placeholders);
+                break;
+            case "save":
+                WordlistManager.save();
+                Message.send(sender, Message.COMMAND_SAVE_SUCCESS, placeholders);
                 break;
             case "add":
+                // Checking permissions
+                if (!sender.hasPermission("chatfilter.command.add")) {
+                    Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
+                    return true;
+                }
+                
                 // Validating arguments
                 if (args.length != 2) {
-                    sender.sendMessage(Component.text("Usage: /chatfilter add <word>", NamedTextColor.RED));
-                    break;
-                }
-                break;
-            case "list":
-                // Validating arguments
-                if (args.length != 1) {
-                    sender.sendMessage(Component.text("Usage: /chatfilter list", NamedTextColor.RED));
+                    Message.send(sender, Message.COMMAND_ADD_CORRECT_USAGE, placeholders);
                     break;
                 }
 
-                sender.sendMessage("%prefix%: The blocked words are:");
+                // Getting placeholders
+                placeholders.put("%word%", args[1]);
+
+                // Adding the word to the wordlist
+                if (WordlistManager.addWord(args[1])) {
+                    Message.send(sender, Message.COMMAND_ADD_WORD_SUCCESS, placeholders);
+                } else {
+                    Message.send(sender, Message.COMMAND_ADD_WORD_FAIL, placeholders);
+                }
+                break;
+            case "list":
+                // Checking permissions
+                if (!sender.hasPermission("chatfilter.command.list")) {
+                    Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
+                    return true;
+                }
+
+                // Validating arguments
+
+                if (args.length > 2) {
+                    Message.send(sender, Message.COMMAND_LIST_CORRECT_USAGE, placeholders);
+                    break;
+                }
+
+                int page = 0;
+                if (args.length == 2) {
+                    try {
+                        page = Integer.parseInt(args[1]) - 1;
+                    } catch (NumberFormatException e) {
+                        Message.send(sender, Message.COMMAND_LIST_CORRECT_USAGE, placeholders);
+                        break;
+                    }
+                }
+
+                // Adding some placeholders
+                placeholders.put("%page%", String.valueOf(page + 1));
+                
+                // Listing the blocked words
+                Message.send(sender, Message.COMMAND_LIST_HEADER, placeholders);
                 StringBuilder builder = new StringBuilder("- ");
                 List<String> blockedWords = WordlistManager.getBlockedWords();
+                List<String> lines = new ArrayList<>();
                 for (int i = 0; i < blockedWords.size(); i++) {
                     // Printing the line and resetting the builder
                     if (builder.length() > 100) {
-                        sender.sendMessage(builder.toString());
+                        lines.add(builder.toString());
                         builder = new StringBuilder("- ");
                     }
 
@@ -98,13 +138,73 @@ public class ChatFilterCommand implements TabExecutor {
                     if (i != blockedWords.size() - 1) builder.append(", ");
                 }
 
+                placeholders.put("%pages%", String.valueOf(lines.size() % 10 + 1));
+
+                if (page > lines.size() / 10 + 1) {
+                    Message.send(sender, Message.COMMAND_LIST_PAGE_TOO_BIG, placeholders);
+                }
+
+                for (int i = page * 10; i < page * 10 + 10; i++) {
+                    if (i >= lines.size()) continue;
+                    sender.sendMessage(lines.get(i));
+                }
+
+                Message.send(sender, Message.COMMAND_LIST_FOOTER, placeholders);
+
                 break;
             case "remove":
+                // Checking permissions
+                if (!sender.hasPermission("chatfilter.command.remove")) {
+                    Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
+                    return true;
+                }
+
                 // Validating arguments
                 if (args.length != 2) {
-                    sender.sendMessage(Component.text("Usage: /chatfilter remove <word>", NamedTextColor.RED));
+                    Message.send(sender, Message.COMMAND_REMOVE_CORRECT_USAGE, placeholders);
                     break;
                 }
+
+                // Getting placeholders
+                placeholders.put("%word%", args[1]);
+
+                // Removing the word to the wordlist
+                if (WordlistManager.removeWord(args[1])) {
+                    Message.send(sender, Message.COMMAND_REMOVE_WORD_SUCCESS, placeholders);
+                } else {
+                    Message.send(sender, Message.COMMAND_REMOVE_WORD_FAIL, placeholders);
+                }
+                break;
+            case "version":
+                // Checking permissions
+                if (!sender.hasPermission("chatfilter.command.version")) {
+                    Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
+                    return true;
+                }
+
+                // Validating arguments
+                if (args.length != 1) {
+                    Message.send(sender, Message.COMMAND_VERSION_CORRECT_USAGE, placeholders);
+                    break;
+                }
+
+                // Collecting placeholders
+                placeholders.put("%plugin_version%", ChatFilter.getPluginVersion());
+                placeholders.put("%latest_version%", ChatFilter.getLatestVersion());
+                
+                // Printing the version number
+                Message.send(sender, ChatFilter.hasUpdate() ? Message.HAS_UPDATE : Message.VERSION_MESSAGE, placeholders);
+                break;
+            default:
+                // Checking permissions
+                if (!sender.hasPermission("chatfilter.command.help")) {
+                    Message.send(sender, Message.NO_PERMISSION, Message.getCommonPlaceholders());
+                    return true;
+                }
+
+                // Sending the help message
+                Message.send(sender, Message.COMMAND_HELP_MESSAGE, placeholders);
+                break;
         }
 
         return true;
@@ -115,17 +215,18 @@ public class ChatFilterCommand implements TabExecutor {
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 1) {
             List<String> suggestions = new ArrayList<>();
-            if (sender.hasPermission("chatfilter.help")) suggestions.add("help");
-            if (sender.hasPermission("chatfilter.add")) suggestions.add("add");
-            if (sender.hasPermission("chatfilter.list")) suggestions.add("list");
-            if (sender.hasPermission("chatfilter.reload")) suggestions.add("reload");
-            if (sender.hasPermission("chatfilter.remove")) suggestions.add("remove");
-            if (sender.hasPermission("chatfilter.version")) suggestions.add("version");
+            if (sender.hasPermission("chatfilter.command.help")) suggestions.add("help");
+            if (sender.hasPermission("chatfilter.command.add")) suggestions.add("add");
+            if (sender.hasPermission("chatfilter.command.list")) suggestions.add("list");
+            if (sender.hasPermission("chatfilter.command.reload")) suggestions.add("reload");
+            if (sender.hasPermission("chatfilter.command.remove")) suggestions.add("remove");
+            if (sender.hasPermission("chatfilter.command.version")) suggestions.add("version");
+            if (sender.hasPermission("chatfilter.command.save")) suggestions.add("save");
             return suggestions.stream().filter(s -> s.startsWith(args[0])).sorted().toList();
         }
 
-        if (args.length == 2 && args[0].equalsIgnoreCase("remove") && sender.hasPermission("chatfilter.remove")) {
-            return WordlistManager.getBlockedWords().stream().filter(s -> s.startsWith(args[2])).sorted().toList();
+        if (args.length == 2 && args[0].equalsIgnoreCase("remove") && sender.hasPermission("chatfilter.command.remove")) {
+            return WordlistManager.getBlockedWords().stream().filter(s -> s.startsWith(args[1])).sorted().toList();
         }
 
         return List.of();
