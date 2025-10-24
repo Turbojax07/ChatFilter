7aa654c4cacffad1fdbe4a5edd6bf4988faca50b
turbojax07 <turbojax07@gmail.com>
2025-10-24 07:16:25 -0400

    Implementing some logging functions

--- src/main/java/org/turbojax/listeners/ChatListener.java
+++ src/main/java/org/turbojax/listeners/ChatListener.java
@@ -1,12 +1,17 @@
 package org.turbojax.listeners;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.turbojax.LogManager;
 import org.turbojax.WordlistManager;
 import org.turbojax.config.MainConfig;
+import org.turbojax.config.Message;
 
 public class ChatListener implements Listener {
     @EventHandler
@@ -26,24 +31,36 @@ public class ChatListener implements Listener {
         // Ignoring if there are no blocked words
         if (blockedWords.isEmpty()) return;
 
+        Map<String,String> placeholders = new HashMap<>(Map.of("%player%", player.getName(), "%message%", message, "%word%", blockedWords.values().iterator().next()));
+
         // Cancelling the event if the config says so
         if (MainConfig.handleChatMessage().equalsIgnoreCase("cancel")) {
             event.setCancelled(true);
 
-            // FILTER_CHAT_CANCEL_LOG("filter-chat-cancel-log", "%prefix%You cannot have the word %word% in the name of your item.")
-            // FILTER_CHAT_CANCEL_MESSAGE("filter-chat-cancel-message", "%prefix%.")
-            // FILTER_CHAT_CANCEL_WARNING("filter-chat-cancel-warning")
+            // Logging
+            LogManager.log(Message.plaintext.serialize(Message.toComponent(Message.FILTER_CHAT_CANCEL_LOG, placeholders)));
+
+            placeholders.putAll(Message.getCommonPlaceholders());
+            Message.send(player, Message.FILTER_CHAT_CANCEL_MESSAGE, placeholders);
+            Message.send(player, Message.FILTER_CHAT_CANCEL_WARNING, placeholders);
         }
 
         // Censoring the message if the config says so
         if (MainConfig.handleChatMessage().equalsIgnoreCase("censor")) {
             for (Entry<Integer,String> blockedWord : blockedWords.entrySet()) {
-                event.setMessage(message.substring(0, blockedWord.getKey()) + "*".repeat(blockedWord.getValue().length()) + message.substring(blockedWord.getKey() + blockedWord.getValue().length()));
+                message = message.substring(0, blockedWord.getKey()) + "*".repeat(blockedWord.getValue().length()) + message.substring(blockedWord.getKey() + blockedWord.getValue().length());
             }
 
-            // FILTER_CHAT_CENSOR_LOG("filter-chat-filter-log", "%prefix%You cannot have the word %word% in the name of your item.")
-            // FILTER_CHAT_CENSOR_MESSAGE("filter-chat-filter-message", "%prefix%You cannot have the word %word% in the name of your item.")
-            // FILTER_CHAT_CENSOR_WARNING("filter-chat-filter-warning")
+            player.chat(message);
+            event.setCancelled(true);
+
+            // Logging
+            placeholders.put("%new_message%", message);
+            LogManager.log(Message.plaintext.serialize(Message.toComponent(Message.FILTER_CHAT_CENSOR_LOG, placeholders)));
+
+            placeholders.putAll(Message.getCommonPlaceholders());
+            Bukkit.broadcast(Message.toComponent(Message.FILTER_CHAT_CENSOR_MESSAGE, placeholders), "chatfilter.notify");
+            Message.send(player, Message.FILTER_CHAT_CENSOR_WARNING, placeholders);
         }
     }
 }
